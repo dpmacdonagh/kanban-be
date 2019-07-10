a008be977c04d5243f3e6cacfd2ca06ae9a561c4
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-09 20:38:08 -0600

    Users controller with create route

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      token = JsonWebToken.encode(user_id: @user.id)
+      render json: { user: @user, token: token }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+        status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
