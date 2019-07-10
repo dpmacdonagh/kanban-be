9f5a1ce86fc5dc3c57a556c5d83435817f7dd99d
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-10 14:05:11 -0600

    Add show action to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,18 @@ class ProjectsController < ApplicationController
   before_action :authorize_request
 
   def create
-    @project = @current_user.projects.create(create_params)
-    render json: @project, status: :created
+    project = @current_user.projects.create(create_params)
+    render json: project, status: :created
+  end
+
+  def show
+    project = @current_user.projects.find(params[:id])
+
+    if project
+      render json: project, status: :ok
+    else
+      render status: :not_found
+    end
   end
 
   private
