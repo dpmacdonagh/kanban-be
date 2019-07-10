fa480005c2eae11a120f0792fee21586391b85ef
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-10 14:07:27 -0600

    Authorize all requests

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,4 @@
 class ProjectsController < ApplicationController
-  before_action :authorize_request
-
   def create
     project = @current_user.projects.create(create_params)
     render json: project, status: :created
