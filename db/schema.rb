38b5eaa8db9a3eaf3f3ca1fa0ba228a4f20b822a
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-10 14:05:05 -0600

    Initial todos model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_025157) do
+ActiveRecord::Schema.define(version: 2019_07_10_200055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_07_10_025157) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
+  create_table "todos", force: :cascade do |t|
+    t.string "name"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_todos_on_project_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_07_10_025157) do
   end
 
   add_foreign_key "projects", "users"
+  add_foreign_key "todos", "projects"
 end
