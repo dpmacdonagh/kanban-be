004859942fcf63c70b57bddf00b4540d5cb0856e
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-18 21:13:05 -0600

    Return header for request specs

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -1,6 +1,6 @@
 module ApiHelper
   def authorize_header(user)
     token = JsonWebToken.encode(user_id: user.id)
-    request.headers.merge!({ 'Authorization': "Bearer #{token}"})
+    { 'Authorization': "Bearer #{token}"}
   end
 end
