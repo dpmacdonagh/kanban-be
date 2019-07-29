ff928af5d66613b467427f9273d44352db60edfc
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-28 19:25:39 -0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :boards
+
+  validates_presence_of :name, :email, :password, :password_confirmation
+  validates_uniqueness_of :email
+  validates :password, confirmation: true
 end
