3502c28cd171c71e0e39cd51d334da771f65652a
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-28 19:25:25 -0600

    Add current to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    collection do
+      get 'current'
+    end
+  end
   resources :boards do
     resources :cards
   end
